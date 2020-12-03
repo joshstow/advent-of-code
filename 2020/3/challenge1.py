b71eab9dc7338e29f4ebea592dfd0687b7295d88
Josh Stow <55459228+joshstow@users.noreply.github.com>
2020-12-03 15:24:13 +0000

    Completed Challenge 1

---
+++ 2020/3/challenge1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  3 12:40:59 2020
+
+@author: joshstow
+"""
+
+import collections
+
+with open('inputs.txt', 'r') as file:   # Open file of puzzle inputs
+    inputs = file.read()   # Read text from file into variable
+    
+inputs = inputs.split('\n')     # Split string on each new line char
+Coord = collections.namedtuple('Coord', 'x y')
+vector = Coord(x=3,y=1)     # Write angle as vector
+
+# Calculate number each element should be multiplied by
+mul = int(((len(inputs)/vector.y)*vector.x)//len(inputs[0]))
+if ((len(inputs)/vector.y)*vector.x)%len(inputs[0]) != 0:
+    mul += 1
+    
+l = [line*mul for line in inputs]   # Extend items in list by factor of multiplier
+
+# Declare increment variables
+y = 0
+x = 0
+answer = 0
+while True:     # Loop through entire list
+    try:
+        if l[y][x] == '#':  # Evaluate if tree encountered
+            answer += 1
+        # Move "pointer"
+        y += vector[1]
+        x += vector[0]
+    except:
+        break
+    
+print(answer)   # 187
