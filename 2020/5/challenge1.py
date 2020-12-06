a9f9e36ccd35d2863866802bb468ed71c8677d58
Josh Stow <55459228+joshstow@users.noreply.github.com>
2020-12-06 00:46:35 +0000

    Completed Challenge 1

---
+++ 2020/5/challenge1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 21:25:58 2020
+
+@author: joshstow
+"""
+
+def findSeat(boarding_pass_tup, rows, cols):
+    for char in boarding_pass_tup[0]:
+        if char == 'F':
+            rows = splitList(rows)[0]   # Keep first half of list
+        else:
+            rows = splitList(rows)[1]   # Keep second half of list
+    for char in boarding_pass_tup[1]:
+        if char == 'L':
+            cols = splitList(cols)[0]   # Keep first half of list
+        else:
+            cols = splitList(cols)[1]   # Keep second half of list
+    return (rows.pop(), cols.pop()) # Take remaining values and remove from list
+
+def splitList(list):
+    return (list[:len(list)//2], list[len(list)//2:])   # Return tuple of each half of list
+
+with open('inputs.txt', 'r') as file:   # Open file of puzzle inputs
+    inputs = file.read()   # Read text from file into variable
+    
+inputs = inputs.split('\n')     # Split string on each new line char
+
+# Create new list with tuples of column and row values
+l = []
+for item in inputs:
+    l.append((item[:7],item[-3:]))
+
+# Create strings of all rows and columns
+total_rows = [row for row in range(128)]
+total_cols = [col for col in range(8)]
+
+# Create new list for seat IDs
+ids = []
+for item in l:
+    a = findSeat(item, total_rows, total_cols)
+    ids.append((a[0]*8)+a[1])   # Calculate ID and append to list
+    
+answer = max(ids)   # Find highest ID
+print(answer)   # 828
+
+            
+        
