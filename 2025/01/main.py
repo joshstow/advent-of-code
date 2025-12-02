8791695f06536e3d78fdea27bcac726e457823cd
Josh <joshxstow@gmail.com>
2025-12-02 01:46:06 +0000

    clean up code and comments

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -2,34 +2,37 @@
 @author: github.com/joshstow
 """
 
-with open("puzzle_input.txt", "r") as f:  # Open puzzle input file
-    puzzle_input = f.read().split("\n")   # Split file contents into list by new lines
-    rotations = [(rotation[0], int(rotation[1:])) for rotation in puzzle_input] # Seperate directional component from distance component 
-
+# Open puzzle input file
+with open("puzzle_input.txt", "r") as f:
+    # Split file contents into list by new lines
+    puzzle_input = f.read().split("\n")
+    # Seperate directional component from distance component 
+    rotations = [(rotation[0], int(rotation[1:])) for rotation in puzzle_input]
 
 def part1():
-    pos = 50    # Starting position
+    pos = 50        # Starting position
     zero_count = 0  # Number of times we land on zero
-    
-    for direction, distance in rotations:   # Iterate through all rotation instructions
+
+    # Iterate through all rotation instructions
+    for direction, distance in rotations:   
+        # Update position using modular arithmetic so value can "wrap around"
         if direction == 'L':
             pos = (pos - distance) % 100
-
         if direction == 'R':
             pos = (pos + distance) % 100
-
         if pos == 0:
             zero_count += 1
 
     return zero_count
 
-
 def part2():
-    pos = 50    # Starting position
+    pos = 50        # Starting position
     zero_count = 0  # Number of times we land on zero
     
-    for direction, distance in rotations:   # Iterate through all rotation instructions    
-        start_pos = pos % 100   # Normalise position to within 0-99
+    # Iterate through all rotation instructions
+    for direction, distance in rotations:
+        # Normalise position to within 0-99
+        start_pos = pos % 100   
 
         # Calculate distance from zero position
         # Moving right: From start_pos up to 100 (wraps to zero)
@@ -48,8 +51,6 @@ def part2():
             pos = (pos + distance) % 100
 
     return zero_count
-
-
 
 if __name__ == "__main__":
     answer1 = part1()
