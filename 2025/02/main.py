7acba156b6f4fc47998a3dc3cefc270177d73c34
Josh <joshxstow@gmail.com>
2025-12-02 15:33:20 +0000

    part 2 solution

--- 2025/02/main.py
+++ 2025/02/main.py
@@ -7,7 +7,6 @@ with open("puzzle_input.txt", "r") as f:
     puzzle_input = f.read().split(',')
     ranges = [tuple(map(int, item.split('-'))) for item in puzzle_input]
 
-
 def part1():
     invalid_total = 0
 
@@ -21,8 +20,21 @@ def part1():
 
     return invalid_total
 
+def part2():
+    invalid_total = 0
+    for start, end in ranges:
+        for i in range(start, end+1):
+            str_i = str(i)
+            # Check if str_i has repeating pattern by searching for it in (str_i + str_i)
+            # If found before midpoint, indicates repeating structure
+            if (str_i + str_i).index(str_i, 1) < len(str_i):
+                invalid_total += i
 
+    return invalid_total
 
 if __name__ == "__main__":
     answer1 = part1()
     print(f"Answer 1: {answer1}")
+
+    answer2 = part2()
+    print(f"Answer 2: {answer2}")
