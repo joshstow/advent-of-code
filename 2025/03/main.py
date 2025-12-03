998d669d6f41698ca2389c31a289e851d5f6250a
Josh <joshxstow@gmail.com>
2025-12-03 15:51:46 +0000

    part 2 solution

--- 2025/03/main.py
+++ 2025/03/main.py
@@ -24,6 +24,36 @@ def part1():
 
     return total
 
+def part2():
+    NUM_DIGITS = 12
+    total = 0
+
+    for block in blocks:
+        last_position = 0
+        largest_joltage = ""
+
+        for digit_index in range(NUM_DIGITS):
+            max_digit = '0'
+            # Calculate furthest position we can search while still leaving enough characters for remaining digits
+            search_end = len(block) - NUM_DIGITS + digit_index + 1
+
+            for i in range(last_position, search_end):
+                if block[i] > max_digit:
+                    max_digit = block[i]
+                    last_position = i
+
+            last_position += 1
+            largest_joltage += max_digit
+
+        total += int(largest_joltage)
+
+    return total
+
 if __name__ == "__main__":
     answer1 = part1()
     print(f"Answer 1: {answer1}")
+
+    answer2 = part2()
+    print(f"Answer 2: {answer2}")
+
+    print(blocks)
