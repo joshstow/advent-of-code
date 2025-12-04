33902f3c6d72b4a0716393801019f66fba9ce927
Josh <joshxstow@gmail.com>
2025-12-04 13:21:05 +0000

    clean up code and comments

--- 2025/04/main.py
+++ 2025/04/main.py
@@ -3,17 +3,17 @@
 """
 
 # Open puzzle input file
-with open("puzzle_input.txt", "r") as f:
-    # Split file contents by newlines into list
+with open("puzzle_input.txt", 'r') as f:
+    # Split file contents by newlines into list of strings
     grid = f.read().split('\n')
 
 def paper_at_position(grid, y, x):
+    # Return False if position is out of bounds
     if (y < 0) or (x < 0) or (y > len(grid)-1) or (x > len(grid[0])-1):
         return False
 
     if grid[y][x] == '@':
         return True
-    
     else:
         return False
 
@@ -26,12 +26,14 @@ def part1():
 
             # If there is a roll of paper at this grid position
             if grid[y][x] == '@':
+                # Count number of paper rolls in adjacent positions
                 for cy in range(-1, 2):
                     for cx in range(-1, 2):
                         if not(cy == 0 and cx == 0):
                             if paper_at_position(grid, y+cy, x+cx):
                                 adjacent_total += 1
 
+                # Paper is accessible if it has fewer than 4 adjacent neighbours
                 if adjacent_total < 4:
                     total_accessible += 1
 
@@ -41,6 +43,7 @@ def part2():
     total_removed = 0
     has_removals = True
 
+    # Create mutable copy of grid (list of char lists)
     mutable_grid = [list(row) for row in grid]
 
     while has_removals == True:
@@ -52,18 +55,21 @@ def part2():
 
                 # If there is a roll of paper at this grid position
                 if mutable_grid[y][x] == '@':
+                    # Count number of paper rolls in adjacent positions
                     for cy in range(-1, 2):
                         for cx in range(-1, 2):
                             if not(cy == 0 and cx == 0):
                                 if paper_at_position(mutable_grid, y+cy, x+cx):
                                     adjacent_total += 1
 
+                    # Remove paper if it has fewer than 4 adjacent neighbours
                     if adjacent_total < 4:
                         removed_this_iteration += 1
                         mutable_grid[y][x] = '.'
 
         total_removed += removed_this_iteration
 
+        # Stop when no more paper can be removed
         if removed_this_iteration == 0:
             has_removals = False
 
