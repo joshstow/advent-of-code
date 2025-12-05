17c550a632692dec98ba474cb11c0aae4a182651
Josh <joshxstow@gmail.com>
2025-12-05 16:15:13 +0000

    add comments

--- 2025/05/main.py
+++ 2025/05/main.py
@@ -6,14 +6,18 @@
 with open("puzzle_input.txt", 'r') as f:
     # Split file contents by double newline to seperate ranges from the ids
     ranges, ids = f.read().split('\n\n')
+    # Parse ranges into list of (lower, upper) tuples
     ranges = [tuple(map(int, line.split('-'))) for line in ranges.split('\n')]
+    # Parse ids into list of integers
     ids = [int(id) for id in ids.split('\n')]
 
 def part1():
     total = 0
 
+    # Check id against all ranges
     for id in ids:
         for lower, upper in ranges:
+            # If id is within range, increment total
             if lower <= id <= upper:
                 total += 1
                 break
@@ -21,18 +25,25 @@ def part1():
     return total
 
 def part2():
+    # Sort ranges by lower bound
     sorted_ranges = sorted(ranges)
 
     total = 0
+    # Start with first range as current merged range
     current_lower, current_upper = sorted_ranges[0]
 
     for lower, upper in sorted_ranges[1:]:
+        # If a gap is found
         if lower > current_upper + 1:
+            # Add current merged range to total and start new range
             total += current_upper - current_lower + 1
             current_lower, current_upper = lower, upper
+        # If ranges touch or overlap
         else:
+            # Extend current range
             current_upper = max(current_upper, upper)
 
+    # Add final merged range to total
     total += current_upper - current_lower + 1
 
     return total
