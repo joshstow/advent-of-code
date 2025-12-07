eebcb27f13c52817b1673d8a99e9441606d00ae6
Josh <joshxstow@gmail.com>
2025-12-07 13:43:29 +0000

    part 2 solution

--- 2025/07/main.py
+++ 2025/07/main.py
@@ -23,6 +23,25 @@ def part1():
 
     return total
 
+def part2():
+    wavefront = [0] * len(grid[0])
+    beam_idx = grid[0].index('S')
+    wavefront[beam_idx] = 1
+
+    for row in grid[1:]:
+        for col_idx in range(len(row)):
+            current_beams = wavefront[col_idx]
+
+            if row[col_idx] == '^' and current_beams != 0:
+                wavefront[col_idx] -= current_beams
+                wavefront[col_idx-1] += current_beams
+                wavefront[col_idx+1] += current_beams
+
+    return sum(wavefront)
+
 if __name__ == "__main__":
     answer1 = part1()
     print(f"Answer 1: {answer1}")
+
+    answer2 = part2()
+    print(f"Answer 2: {answer2}")
